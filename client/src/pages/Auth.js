5c1cafd0ffc39d8147f687a84c783bc7dbfc7070
Ruslansergeevich96 <mascot.tihov@yandex.ru>
2022-04-26 23:46:37 +0300

    lates

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -47,6 +47,14 @@ const Auth = () => {
                 {isLogin ? 'Войти' : 'Регистрация'}
               </Button>
             </Row>
+            <Nav defaultActiveKey="/home" className="flex-column">
+  <Nav.Link href="/home">Active</Nav.Link>
+  <Nav.Link eventKey="link-1">Link</Nav.Link>
+  <Nav.Link eventKey="link-2">Link</Nav.Link>
+  <Nav.Link eventKey="disabled" disabled>
+    Disabled
+  </Nav.Link>
+</Nav>
         </Form>
       </Card>
     </Container>
