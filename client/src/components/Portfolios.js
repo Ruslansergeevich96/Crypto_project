9b305b4e70c9efc1639fd3aa4aa6ef86f5e12e00
Ruslansergeevich96 <mascot.tihov@yandex.ru>
2022-04-26 23:56:00 +0300

    lates

--- client/src/components/Portfolios.js
+++ client/src/components/Portfolios.js
@@ -1,11 +1,13 @@
-import React from "react";
+import { observable } from "mobx";
+import React, { useContext } from "react";
 
-const Portfolios = () => {
+const Portfolios = observable(() => {
+    const {} = useContext()
     return (
         <div>
             Portfolios
         </div>
     );
-};
+});
 
 export default Portfolios;
