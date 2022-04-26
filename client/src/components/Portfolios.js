66afaf91dd717ca775da5609672628be04a58306
Ruslansergeevich96 <mascot.tihov@yandex.ru>
2022-04-27 00:00:59 +0300

    lates

--- client/src/components/Portfolios.js
+++ client/src/components/Portfolios.js
@@ -1,13 +1,22 @@
 import { observable } from "mobx";
 import React, { useContext } from "react";
+import { Context } from "..";
+import ListGroup from "react-bootstrap/ListGroup"
 
 const Portfolios = observable(() => {
-    const {} = useContext()
+    const {} = useContext(Context)
     return (
-        <div>
-            Portfolios
-        </div>
+        <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
     );
+    <div>
+            Portfolios
+    </div>
 });
 
 export default Portfolios;
