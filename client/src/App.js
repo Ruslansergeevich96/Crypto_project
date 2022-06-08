edc07ba111859929f8adfd1296306f20f5eefae0
Ruslansergeevich96 <mascot.tihov@yandex.ru>
2022-06-08 23:53:17 +0300

    lates

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import React, { useContext, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Context } from '.';
+import { check } from '../../server/controllers/userController';
 import AppRouter from './components/AppRouter';
 import LeftNavs from './components/LeftNavs';
 import NavBar from './components/NavBar';
+import { Spinner } from "react-bootstrap";
 
 
 
-const App = () => {
+const App = observer( () => {
+  const {user} = useContext(Context)
+  const [loading, setLoading] = useState(true)
+
+  useEffect( () => {
+    check().then(data => {
+      user.setUser(true)
+      user.setIsAuth(true)
+    }).finally( () => setLoading(false))
+  }, [])
+
+  if (loading) {
+    return <Spinner animation = {"grow"}/>
+  }
+
   return (
     <BrowserRouter>
       <NavBar />
@@ -15,6 +33,6 @@ const App = () => {
       WORKING APP.JS
     </BrowserRouter>
   );
-}
+});
 
 export default App;
